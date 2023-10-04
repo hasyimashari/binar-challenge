41071bdc0b8ecb68237103854cafe94a62b4aee5
mhsy <hasyim11c@gmail.com>
2023-10-05 00:39:57 +0700

    [ch-4] add: add rest api handler and implemented it in index.js end point

---
+++ challenge-04/handler/handler.js
@@ -0,0 +1,101 @@
+const { where } = require("sequelize");
+const {car} = require("../models");
+
+const getPing = (req, res) => {
+    res.status(200).json({message: "ping succesfully"});
+};
+
+const getListCars = async(req, res) => {
+    try {
+        const data =  await car.findAll();
+        res.status(200).json({
+            message: "get data succeeded",
+            data: data});
+
+    } catch(error) {
+        res.status(500).json({
+            error: error.message
+        });
+    };
+};
+
+const getCar = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const data =  await car.findByPk(id);
+        res.status(200).json({
+            message: "get data succeeded",
+            data: data});
+
+    } catch(error) {
+        res.status(500).json({
+            error: error.message
+        });
+    };
+};
+
+const createCar = async(req, res) => {
+    try {
+        const newData = req.body;
+        const newCar =  await car.create(newData);
+
+        res.status(201).json({
+            message: "new data successfully created",
+            newData: newCar
+        });
+
+    } catch(error) {
+        res.status(500).json({
+            error: error.message
+        });
+    };
+};
+
+const updateCar = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const newData = req.body;
+
+        const [,updatedCar] =  await car.update(newData, {where : {id} ,returning: true});
+
+        res.status(201).json({
+            message: "data successfully updated",
+            newData: updatedCar
+        });
+
+    } catch(error) {
+        res.status(500).json({
+            error: error.message
+        });
+    };
+};
+
+const deleteCar = async(req, res) => {
+    try {
+        const {id} = req.params;
+        await car.destroy({ where: {id} });
+
+        res.status(200).json({
+            message: "data successfully deleted"
+        });
+
+    } catch(error) {
+        res.status(500).json({
+            error: error.message
+        });
+    };
+};
+
+const notFound = (req, res) => {
+    res.status(404).json({ message: "end point not found or wrong method" })
+}
+
+module.exports = {
+    getPing,
+    getListCars,
+    getCar,
+    createCar,
+    updateCar,
+    deleteCar,
+    notFound,
+}
--- challenge-04/index.js
+++ challenge-04/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const handler = require('./handler/handler');
 
 const app = express();
 const PORT = '3000';
@@ -8,3 +9,15 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`open http://127.0.0.1:${PORT}`)
 });
+
+app.get('/', handler.getPing);
+app.get('/cars', handler.getListCars);
+app.get('/cars/:id', handler.getCar);
+app.post('/cars', handler.createCar);
+app.put('/cars/:id', handler.updateCar);
+app.delete('/cars/:id', handler.deleteCar);
+
+app.get('*', handler.notFound);
+app.post('*', handler.notFound);
+app.put('*', handler.notFound);
+app.delete('*', handler.notFound);
