4dcb6de2661fa3f8cd75016fb5da4c77a3fa13b6
mhsy <hasyim11c@gmail.com>
2023-10-20 00:19:31 +0700

    [ch-5] add user migration, user model, and super admin seeder

---
+++ challenge-05/db/seeders/20231019170323-generate-superAdmin.js
@@ -0,0 +1,41 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('users', [
+      {
+        name: "super admin 1",
+        email: "super@admin1.com",
+        encryptedPassword: bcrypt.hashSync("super_admin111", 10),
+        phone: "085880880880",
+        addres: "kediri",
+        role: "superadmin",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('users', null, {})
+  }
+};
